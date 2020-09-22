5f0a0b103fb68d9724b7656b14fd3c454f8d5369
mnpenchev <mnpenchev@gmail.com>
2020-09-22 13:17:02 +0100

    migrated the DB to postgresql

--- DjangoPortfolio/settings.py
+++ DjangoPortfolio/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'DjangoPortfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DjangoPortfolio',
+        'USER': 'postgres',
+        'PASSWORD': 'password1',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
