f9447f5dd7c69c6b1b182d566c2f1e08f30c2054
mnpenchev <mnpenchev@gmail.com>
2020-09-22 15:57:07 +0100

    added functional home page with bootstrap for Jobs app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+# Create a Blog modules. V
+# title V
+# publication date V
+# body V
+# image V
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    publication_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+# add the blog app to the setting. V
+
+# create migration V
+
+# migrate V
+
+# add to the admin
