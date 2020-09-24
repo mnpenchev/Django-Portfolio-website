aa9df78881886112bac474499f7b441085129e53
mnpenchev <mnpenchev@gmail.com>
2020-09-24 09:54:48 +0100

    blog functionality and polish

--- DjangoPortfolio/settings.py
+++ DjangoPortfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'DjangoPortfolio',
         'USER': 'postgres',
-        'PASSWORD': 'password1',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
